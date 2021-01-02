4bc1dab0a9da6b6158e3437705bb0018e973eff0
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-01-01 15:35:00 -1000

    Implemented search engine functionality, added WeatherInfo component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import Loader from 'react-loader-spinner';
 import Forecast from "./Forecast";
@@ -8,6 +8,7 @@ import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -26,10 +27,25 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "28aa11ac2c3547ae8dd36de6f31e399a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
     <div className="Weather">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-row align-items-center">
           <div className="col-10">
             <input
@@ -38,13 +54,13 @@ export default function Weather(props) {
               placeholder="Enter a city"
               autoFocus="on"
               autoComplete="off"
+              onChange={handleCityChange}
             />
           </div>
           <div className="col-1">
             <button
               type="submit"
-              className=" form-control-sm btn btn-sm btn-light shadow-sm border-0 searchIcon"
-            >
+              className="form-control-sm btn btn-sm btn-light shadow-sm border-0 searchIcon">
               <i className="fas fa-search search-icon" />
             </button>
           </div>
@@ -55,68 +71,12 @@ export default function Weather(props) {
           </div>
         </div>
       </form>
-      <div className="row">
-        <div className="col">
-          <h1>
-            <strong>{weatherData.city}</strong>
-          </h1>
-        </div>
-      </div>
-      <div className="currentDateTime"><FormattedDate date={weatherData.date} /></div>
-      <div className="description">{weatherData.description}</div>
-      <span className="currentTemperature">{Math.round(weatherData.temperature)}</span>
-          <span className="units">
-            <a href="/" className="active">
-              °F
-            </a>{" "}
-            |<a href="/">°C</a>
-          </span>
-          <div className="highLowTemps">
-              <i className="fas fa-long-arrow-alt-up" />{" "}
-              <span className="maxTemp">{Math.round(weatherData.high)}</span>° 
-              {" "}<i className="fas fa-long-arrow-alt-down" />{" "}
-              <span className="minTemp">{Math.round(weatherData.low)}</span>°
-          </div> 
-
-      <div className="weatherWrapper">  
-        <div className="row row-cols-2">
-          <div className="col">
-            <img src={weatherData.imgUrl} alt={weatherData.description} className="currentTempIcon" />
-          </div>  
-          <div className="col">
-            <div className="details">DETAILS</div>
-            <ul>
-              <div className="weatherToday">
-                <div className="feelsLike">
-                  <li><i className="fas fa-temperature-low"></i> Feels Like:{" "}
-                  <span className="feelsLike">{Math.round(weatherData.feelsLike)}</span>°</li>
-                </div>
-
-                <div className="sunrise-sunset">
-                  <li><i class="fas fa-sun"></i> Sunrise: <span>{weatherData.sunrise}</span> {" "}</li> 
-                  <li><i class="fas fa-moon"></i> Sunset:{" "}
-                  <span>{weatherData.sunset}</span></li>
-                </div>
-
-                <div className="humidity-wind">
-                  <li><i className="fas fa-tint" /> Humidity:{" "}
-                  <span className="humidity">{weatherData.humidity}</span>% {" "}</li>
-                  <li><i className="fas fa-wind" /> Wind:{" "}
-                  <span className="wind"> {Math.round(weatherData.wind)} mph </span></li>
-                </div>
-              </div> 
-            </ul>
-          </div>    
-        </div> 
-      </div>
-    <Forecast />
+      <WeatherInfo data={weatherData} /> 
+      <Forecast />
     </div>
   );
   } else {
-      const apiKey = "28aa11ac2c3547ae8dd36de6f31e399a";
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
-      axios.get(apiUrl).then(handleResponse);
-
+      search();
       return (
         <Loader type="Bars" color="#2b2f70" height={60} width={60} />
       );
---
+++ src/WeatherInfo.js
@@ -0,0 +1,61 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+    <div className="row">
+        <div className="col">
+          <h1>
+            <strong>{props.data.city}</strong>
+          </h1>
+        </div>
+      </div>
+      <div className="currentDateTime"><FormattedDate date={props.data.date} /></div>
+      <div className="description">{props.data.description}</div>
+      <span className="currentTemperature">{Math.round(props.data.temperature)}</span>
+      <span className="units">
+        <a href="/" className="active">°F</a>{" "}
+        |<a href="/">°C</a>
+      </span>
+      <div className="highLowTemps">
+        <i className="fas fa-long-arrow-alt-up" />{" "}
+        <span className="maxTemp">{Math.round(props.data.high)}</span>° 
+        {" "}<i className="fas fa-long-arrow-alt-down" />{" "}
+        <span className="minTemp">{Math.round(props.data.low)}</span>°
+      </div> 
+
+      <div className="weatherWrapper">  
+        <div className="row row-cols-2">
+          <div className="col">
+            <img src={props.data.imgUrl} alt={props.data.description} className="currentTempIcon" />
+          </div>  
+          <div className="col">
+            <div className="details">DETAILS</div>
+            <ul>
+              <div className="weatherToday">
+                <div className="feelsLike">
+                  <li><i className="fas fa-temperature-low"></i> Feels Like:{" "}
+                  <span className="feelsLike">{Math.round(props.data.feelsLike)}</span>°</li>
+                </div>
+
+                <div className="sunrise-sunset">
+                  <li><i className="fas fa-sun"></i> Sunrise: <span>{props.data.sunrise}</span> {" "}</li> 
+                  <li><i className="fas fa-moon"></i> Sunset:{" "}
+                  <span>{props.data.sunset}</span></li>
+                </div>
+
+                <div className="humidity-wind">
+                  <li><i className="fas fa-tint" /> Humidity:{" "}
+                  <span className="humidity">{props.data.humidity}</span>% {" "}</li>
+                  <li><i className="fas fa-wind" /> Wind:{" "}
+                  <span className="wind"> {Math.round(props.data.wind)} mph </span></li>
+                </div>
+              </div> 
+            </ul>
+          </div>    
+        </div> 
+      </div>
+    </div>  
+  );
+}
